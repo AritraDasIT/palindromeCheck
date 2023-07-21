b8892751cfaddfec4feca87808d374b73f98df07
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-07-21 16:40:53 +0530

    Update palindrome_string.c

--- palindrome_string.c
+++ palindrome_string.c
@@ -1,40 +1,28 @@
-#include<stdio.h>
-int length(char str[10])
-{
-	int i=0;
-	
-	while(str[i]!='\0')
-	{
-		i++;
-	}
-	return i ;
-}
+#include <stdio.h>
+#include <string.h>
 
-int main()
-{
-	char arr[10];
-	int i=0;
-	int len;
-	len=length(arr);
-	int flag=0;
-	
-	printf("Enter the string: ");
-	scanf("%s",arr);
-	
-	for(i=0;i<len;i++)
-	{
-		if(arr[i]!=arr[len-i-1])
-		{
-			flag=1;
-			break;
-		}
-	}
-	if(flag==1)
-	{
-		printf("%s is not a palindrome ",arr);
-	}
-	else
-	{
-		printf("%s is a palindrome",arr);
-	}
+int main(){
+    char string1[20];
+    int i, length;
+    int flag = 0;
+    
+    printf("Enter a string:");
+    scanf("%s", string1);
+    
+    length = strlen(string1);
+    
+    for(i=0;i < length ;i++){
+        if(string1[i] != string1[length-i-1]){
+            flag = 1;
+            break;
+           }
+        }
+    
+    if (flag) {
+        printf("%s is not a palindrome", string1);
+    }    
+    else {
+        printf("%s is a palindrome", string1);
+    }
+    return 0;
 }
